61aa5f43c5e5aba1fc6d2634451c677aead50614
paulinatrillo <paulina.trillo@hotmail.com>
2025-03-28 14:59:51 -0300

    Agregado botón para eliminar producto y persistencia en archivo

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -13,5 +13,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-module.exports = router;
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await manager.getProducts();
+    const productIndex = products.findIndex(product => product._id === id);
 
+    if (productIndex === -1) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
+    products.splice(productIndex, 1);
+    await manager.saveProducts(products);
+
+    res.status(200).json({ message: "Producto eliminado" });
+  } catch (error) {
+    res.status(500).json({ error: "Error al eliminar el producto", message: error.message });
+  }
+});
+
+module.exports = router;
